42b6e98953648fd877b52511cc1875a9470ce7e8
Stefan Copetchi <kopezaur@gmail.com>
2018-04-27 18:02:15 +0200

    updated initializer for Map

--- src/MapServer.c
+++ src/MapServer.c
@@ -69,7 +69,7 @@ void defaultMapsGeneration(void) {
 							{ 0, 0, 0, 2, 2, 2, 2, 2, 0, 0 },
 							{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 							{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
-	struct Map map1 = { matrix1, op1 };
+	struct Map map1 = { .matrix = matrix1, .objPos = op1 };
 	maps[0] = map1;
 }
 
