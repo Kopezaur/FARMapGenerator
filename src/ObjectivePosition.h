c2e929c4913ad280553b94619131851eda67d27b
Stefan Copetchi <kopezaur@gmail.com>
2018-04-27 17:13:54 +0200

    removed getters and setters

--- src/ObjectivePosition.h
+++ src/ObjectivePosition.h
@@ -9,43 +9,4 @@ struct ObjectivePosition
     Position thebes;
     Position oedipe;
     Position sphinx;
-
-    //Getters and Setters
-    void set_nbVillager (int n)
-    {
-      nbVillager = n;
-    }
-
-    int get_nbVillager (void)
-    {
-      return nbVillager;
-    }
-
-    void set_thebes (Position n)
-    {
-      thebes = n;
-    }
-
-    int get_thebes (void)
-    {
-      return thebes;
-    }
-    void set_oedipe (Position n)
-    {
-      oedipe = n;
-    }
-
-    int get_oedipe (void)
-    {
-      return oedipe;
-    }
-    void set_sphinx (Position n)
-    {
-      sphinx = n;
-    }
-
-    int get_sphinx(void)
-    {
-      return sphinx;
-    }
 };
